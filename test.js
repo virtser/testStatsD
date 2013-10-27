66dce5d217c72235d0bd6e629c99355aa09d2c97
David Virtser <poison77@gmail.com>
2013-10-27 09:58:25 +0200

    title change

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var dgram = require('dgram');
-var message = new Buffer("test:1|c");
+var message = new Buffer("SimpleStatsDTestFromNode:1|c");
 var client = dgram.createSocket("udp4");
 client.send(message, 0, message.length, 8125, "127.0.0.1", function(err, bytes) {
   client.close();
